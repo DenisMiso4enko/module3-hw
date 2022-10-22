00cd674572296a5004ebf989496da181e91ae610
DenisMiso4enko <derrmisochenko17@gmail.com>
2022-10-22 22:48:20 +0300

    add solution for hw-4

---
+++ hw-4/interfases.ts
@@ -0,0 +1,28 @@
+export interface INewFilm {
+    id: number | undefined
+    title: string
+    released: string
+    plot: string
+
+}
+
+export interface IFilm {
+    id?: number
+    title: string
+    year: number
+    released: string
+    runtime: string
+    genre: string[]
+    director: string
+    writer: string
+    actors: string[]
+    plot: string
+    country: string
+    poster: string
+    imdbRating: number
+    imdbVotes: number
+    type?: string
+    boxOffice: string
+    production: string
+
+}
